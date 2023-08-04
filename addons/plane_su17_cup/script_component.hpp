5589590ec9d2aae790a8362229f4e15f9c6decd0
erem2k <nope@nohost.com>
2023-08-04 04:49:55 -0400

    Project structure: Move tmtm_su17 to plane_su17

---
+++ addons/plane_su17_cup/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT plane_su17_cup
+#define COMPONENT_BEAUTIFIED Su-17 CUP Compatibility
+#include "\tmtm\aircraft\common\script_mod.hpp"
+#include "\tmtm\aircraft\common\script_macros.hpp"
